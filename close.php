b68a7593d1a609755e0f6c1fd63c6ca85de622eb
sumo0711 <dinosaursumo@gmail.com>
2024-05-31 21:22:02 +0800

    關店頁面

--- close.php
+++ close.php
@@ -31,35 +31,9 @@ $conn->close();
 <html lang="zh-TW">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/close.css">
     <link rel="website icon" type="image/png" href="image/logo.png">
     <title>老師傅</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin: 0;
-            padding: 0;
-			background-image: url("image/006.png") ;
-			background-repeat: no-repeat;
-			background-size: cover;
-        }
-        .container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-			border: 2px solid #6b8e23
-			
-        }
-        .message {
-            padding: 40px;
-            background-color: #cde8e5;
-            border: 1px solid #ccc;
-            box-shadow: 2px 0 10px rgba(0, 0, 0, 0.5);
-			border-radius: 15px; /* 圆角 */
-        }
-    </style>
 </head>
 <body>
     <div class="container">
